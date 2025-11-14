1687734b232b0230726ba43721ebf391345f5e4d
MATPOK <Mateusz.Pokrywka04@gmail.com>
2025-11-14 19:37:44 +0100

    Poprawa DatyUrodzenia na DateOnly z DateTime

---
+++ AplikacjaDoObsady/Migrations/20251114183613_dataurodzenia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AplikacjaDoObsady.Migrations
+{
+    /// <inheritdoc />
+    public partial class dataurodzenia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "MatchDate",
+                table: "Matches",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "MatchDate",
+                table: "Matches",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
--- AplikacjaDoObsady/Models/Referee.cs
+++ AplikacjaDoObsady/Models/Referee.cs
@@ -10,7 +10,9 @@ namespace AplikacjaDoObsady.Models
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string League { get; set; } = string.Empty;
-        public DateTime DateOfBirth { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly DateOfBirth { get; set; }
+        
         public string PhoneNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
 
